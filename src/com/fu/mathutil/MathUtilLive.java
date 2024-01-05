239f319a3c2b7fccd66a28b333959860449f40ad
ngocdung1004 <nguyenngocdung100420032k3@gmail.com>
2024-01-05 09:05:11 +0700

    lan dau dua code len server

---
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MathUtilLive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        long result = MathUtil.getFactorial(5);
+        System.out.println("5! = 120; actual: " + result);
+        
+        System.out.println("6! = 720; actual: " + MathUtil.getFactorial(6));
+        System.out.println("0! = 1; actual: " + MathUtil.getFactorial(0));
+        
+        
+        
+               
+        
+        
+        
+        
+    }
+    
+}
+
